6e77181fea781c3c2dfc118823b45d8c96eaf3d1
Jerry Tang <snaketzy@gmail.com>
2024-01-01 18:58:27 +0800

    Update popup.js

--- connection/popup.js
+++ connection/popup.js
@@ -15,7 +15,7 @@ function setChildTextNode(elementId, text) {
 
 // Tests the roundtrip time of sendMessage().
 const testMessage = async () => {
-  const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+  const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
   const response = await chrome.tabs.sendMessage(tab.id, {greeting: "hello"});
   // do something with response here, not outside the function
   //  alert(response.farewell)
